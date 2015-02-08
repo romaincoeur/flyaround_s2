a69ae55b28fc8d359dbc165a2a16c53b4580de72
romaincoeur <coeurro@gmail.com>
2015-02-08 20:48:37 +0100

    other insight error fixes

--- src/Flyaround/MapBundle/Admin/TerrainAdmin.php
+++ src/Flyaround/MapBundle/Admin/TerrainAdmin.php
@@ -7,7 +7,6 @@ namespace Flyaround\MapBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 
